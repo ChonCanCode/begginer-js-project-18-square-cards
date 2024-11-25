a1bf67c7b6475b0920a69a9b184fd23ad9e15de4
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2024-11-25 15:25:23 +0000

    completed

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=cTj69LZ3KAU")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=cTj69LZ3KAU")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=cTj69LZ3KAU")
+})
+
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://www.fauxhammer.com/top-10/best-pin-vise-hobby-drill-for-miniatures-models/")
+})
+
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=cTj69LZ3KAU")
+})
